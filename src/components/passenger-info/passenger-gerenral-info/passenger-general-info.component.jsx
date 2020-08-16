c260f13cd639a48cd3035ddf723bd2e811034c57
Ayush Jain <ayush.ayushjain12@gmail.com>
2020-08-16 10:21:44 +0530

    Updated passenger General-info for crew logfed-in

--- src/components/passenger-info/passenger-gerenral-info/passenger-general-info.component.jsx
+++ src/components/passenger-info/passenger-gerenral-info/passenger-general-info.component.jsx
@@ -12,18 +12,22 @@ import {selectUnoccupiedSeat} from '../../../store/allpassenger/allpassenger.sel
     const [id,setid] = useState(passengerData.id) 
     const [firstName, setfirstName]    = useState(passengerData.firstName);
     const [lastName, setlastName]      = useState(passengerData.lastName)
-    const [age, setage]         = useState(passengerData.age)
-    const [seatNo, setseatNo]   = useState(passengerData.seatNo)
-    const [PNR, setPNR]         = useState(passengerData.PNR)
+    const [dob, setdob]                = useState(passengerData.dob)
+    const [seatNo, setseatNo]          = useState(passengerData.seatNo)
+    const [PNR, setPNR]                = useState(passengerData.PNR)
+    const [passport, setpassport]      = useState(passengerData.passport)   
+    const [address, setaddress]             = useState(passengerData.address)
     const [contactNumber, setcontactNumber]  = useState(passengerData.contactNumber)
    
     useEffect(()=>{
             setid(passengerData.id)
              setfirstName(passengerData.firstName);
              setlastName(passengerData.lastName)
-             setage(passengerData.age)
+             setdob(passengerData.dob)
              setseatNo(passengerData.seatNo)
              setPNR(passengerData.PNR)
+             setpassport(passengerData.passport)
+             setaddress(passengerData.address)
              setcontactNumber(passengerData.contactNumber)
             // console.log('Passenger Data changes',seatNo)
     },[passengerData])
@@ -31,12 +35,15 @@ import {selectUnoccupiedSeat} from '../../../store/allpassenger/allpassenger.sel
       <Fragment> 
             <div className= 'passenger-general-info-container ' style={{width:`${width}`}}>
                     <div className= 'passenger-heading'>Passenger Info</div>
-                    <div className= 'passenger-info-items'> <span>FirstName</span>  <span>{firstName}</span> </div>
-                    <div className= 'passenger-info-items'> <span>LastName</span>   <span>{lastName}</span> </div>
-                    <div className= 'passenger-info-items'> <span>ContactNo.</span> <span>{contactNumber}</span> </div>
-                    <div className= 'passenger-info-items'> <span>Age</span>        <span>{age}</span> </div>
+                    <div className= 'passenger-info-items'> <span>FirstName</span>  <span>{firstName?firstName :'--'}</span> </div>
+                    <div className= 'passenger-info-items'> <span>LastName</span>   <span>{lastName?lastName :'--'}</span> </div>
+                    <div className= 'passenger-info-items'> <span>SeatNo</span>     <span> {seatNo?seatNo :'--'}</span> </div>
+                    <div className= 'passenger-info-items'> <span>ContactNo.</span> <span>{contactNumber?contactNumber :'--'}</span> </div>
+                    <div className= 'passenger-info-items'> <span>DOB</span>        <span>{dob? dob:'--'}</span> </div>
                     <div className= 'passenger-info-items'> <span>PNR</span>        <span>{PNR}</span> </div>
-                    <div className= 'passenger-info-items'> <span>SeatNo</span>     <span> {seatNo}</span> </div>
+                    <div className= 'passenger-info-items'> <span>Passport</span>   <span> {passport? passport:'--'}</span> </div>
+                    <div className= 'passenger-info-items'> <span>Address</span>    <span style={{paddingLeft: '40px'}}> {address? address:'--'}</span> </div>
+        
             </div>
         </Fragment>
     )
