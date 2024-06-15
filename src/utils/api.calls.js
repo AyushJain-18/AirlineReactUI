07723e24d829d6a81a34b7a28caad1cb14d86164
Ayush Jain <ayush.ayushjain12@gmail.com>
2024-06-15 18:17:44 +0530

    Updating json server url

--- src/utils/api.calls.js
+++ src/utils/api.calls.js
@@ -1,21 +1,20 @@
 import axios from 'axios';
 
 //const serverUrl ='http://localhost:3000';
-const serverUrl =  'https://happy-pullover-deer.cyclic.app';
+const serverUrl = 'https://airlineserver.onrender.com';
 
+export const getRequest = (endpoint) => {
+  return axios.get(serverUrl + endpoint);
+};
 
-export const getRequest =(endpoint)=>{
-  return axios.get(serverUrl+endpoint)
-}
+export const postRequest = (endpoint, payload) => {
+  return axios.post(serverUrl + endpoint, { ...payload });
+};
 
-export const postRequest =(endpoint, payload)=>{
-  return axios.post(serverUrl+endpoint, {...payload})
-}
-
-export const deleteRequest =(endpoint, idForDeleteItem )=>{
-  return axios.delete(serverUrl+endpoint+'/'+idForDeleteItem)
-}
-export const updateRequest =(endpoint, payload)=>{
+export const deleteRequest = (endpoint, idForDeleteItem) => {
+  return axios.delete(serverUrl + endpoint + '/' + idForDeleteItem);
+};
+export const updateRequest = (endpoint, payload) => {
   // console.log(serverUrl+endpoint);
-  return axios.put(serverUrl+endpoint,{...payload})
-}
+  return axios.put(serverUrl + endpoint, { ...payload });
+};
